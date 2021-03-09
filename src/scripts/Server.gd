358f4a73ea9f0446554dbcb1c4d115445d465397
KuroIShiro <pxpxpx2015@gmail.com>
2021-03-09 21:24:02 +0300

    Added basic networking

---
+++ src/scripts/Server.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var network = NetworkedMultiplayerENet.new()
+var port = 1909
+var max_players = 4095
+
+
+func _ready() -> void:
+	StartServer()
+
+
+func StartServer() -> void:
+	network.create_server(port, max_players)
+	get_tree().set_network_peer(network)
+	print("Server started")
+	
+	network.connect("peer_connected", self, "_peer_connected")
+	network.connect("peer_disconnected", self, "_peer_disconnected")
+
+remote func send_units_list() -> void:
+	var peer_id = get_tree().get_rpc_sender_id()
+	var data = ServerData.units_list
+	rpc_id(peer_id, "_return_units_list", data)
+	print("Sending " + str(data) + " to peer id " + str(peer_id))
+
+
+func _peer_connected(id) -> void:
+	print("Peer id " + str(id) + " connected")
+
+func _peer_disconnected(id) -> void:
+	print("Peer id " + str(id) + " disconnected")
---
+++ src/scripts/ServerData.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var units_list = {}
+
+func _ready() -> void:
+#	var units_list_file = File.new()
+#	units_list_file.open("res://Data/units_list.json", File.READ)
+#	var units_list_json = JSON.parse(units_list_file.get_as_text())
+#	units_list_file.close()
+#	units_list = units_list_json.result
+#	print(units_list)
+
+	var units_list_file = File.new()
+	units_list_file.open("res://Data/units_list.json", File.READ)
+	var units_list = parse_json(units_list_file.get_as_text())
+	units_list_file.close()
